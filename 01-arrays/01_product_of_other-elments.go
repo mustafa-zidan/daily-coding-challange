345113021e78e021891b440a4e05d98d2de8705d
Mustafa Zidan <mustafa@zidan.me>
2019-09-24 17:56:24 +0200

    add products of other elements with no division solution
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- 01-arrays/01_product_of_other-elments.go
+++ 01-arrays/01_product_of_other-elments.go
@@ -3,7 +3,7 @@ package arrays
 //Given an array of integers, return a new array such that each element
 //at index i of the new array is the product of all the numbers in the
 //original array except one in i
-func Product(nums []int) []int {
+func productOfTheOthers(nums []int) []int {
 	product := 1
 	for _, val := range nums {
 		if val != 0 {
@@ -17,6 +17,32 @@ func Product(nums []int) []int {
 		} else {
 			result[i] = product / v
 		}
+	}
+	return result
+}
+
+func productOfTheOthersNoDivision(nums []int) []int {
+	n := len(nums)
+	left, right := make([]int, n), make([]int, n)
+	left[0], right[n-1] = 1, 1
+	for i := 1; i < n; i++ {
+		if nums[i-1] != 0 {
+			left[i] = left[i-1] * nums[i-1]
+		} else {
+			left[i] = left[i-1]
+		}
+	}
+	for i := n - 2; i >= 0; i-- {
+		if nums[i+1] != 0 {
+			right[i] = right[i+1] * nums[i+1]
+		} else {
+			right[i] = right[i+1]
+		}
+	}
+
+	result := make([]int, n)
+	for i := range nums {
+		result[i] = left[i] * right[i]
 	}
 	return result
 }
--- 01-arrays/01_product_of_other_elements_test.go
+++ 01-arrays/01_product_of_other_elements_test.go
@@ -8,18 +8,37 @@ import (
 func TestProductOfOtherElements(t *testing.T) {
 	// case 1 example 1
 	input := []int{1, 2, 3, 4, 5}
-	output := Product(input)
+	output := productOfTheOthers(input)
 	assert.Equal(t, []int{120, 60, 40, 30, 24}, output)
 	// case 2 example 2
 	input = []int{3, 2, 1}
-	output = Product(input)
+	output = productOfTheOthers(input)
 	assert.Equal(t, []int{2, 3, 6}, output)
 	// case 3 zero element
 	input = []int{3, 2, 1, 0}
-	output = Product(input)
+	output = productOfTheOthers(input)
 	assert.Equal(t, []int{2, 3, 6, 6}, output)
 	// case 4 two zero elements
 	input = []int{0, 3, 2, 1, 0}
-	output = Product(input)
+	output = productOfTheOthers(input)
+	assert.Equal(t, []int{6, 2, 3, 6, 6}, output)
+}
+
+func TestProductOfOtherElementsNoDevision(t *testing.T) {
+	// case 1 example 1
+	input := []int{1, 2, 3, 4, 5}
+	output := productOfTheOthersNoDivision(input)
+	assert.Equal(t, []int{120, 60, 40, 30, 24}, output)
+	// case 2 example 2
+	input = []int{3, 2, 1}
+	output = productOfTheOthersNoDivision(input)
+	assert.Equal(t, []int{2, 3, 6}, output)
+	// case 3 zero element
+	input = []int{3, 2, 1, 0}
+	output = productOfTheOthersNoDivision(input)
+	assert.Equal(t, []int{2, 3, 6, 6}, output)
+	// case 4 two zero elements
+	input = []int{0, 3, 2, 1, 0}
+	output = productOfTheOthersNoDivision(input)
 	assert.Equal(t, []int{6, 2, 3, 6, 6}, output)
 }
