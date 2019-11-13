f33d2285e9d823e820683ea17043c19965bd7171
Mustafa Zidan <mustafa@zidan.me>
2019-11-13 19:09:37 +0100

    add 3rd problem in Arrays chapter followup solution
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- 01-arrays/03_calculate_maximum_subarray_sum.go
+++ 01-arrays/03_calculate_maximum_subarray_sum.go
@@ -14,3 +14,27 @@ func maxSubarraySum(a []int) int {
 	}
 	return max
 }
+
+//what if the elements can wrap around? for example [8, -1, 3, 4] returns 15
+func maxCircularSubarraySum(a []int) int {
+	maxSum := sum(a) - minSubarraySum(a)
+	return utils.Max(maxSum, maxSubarraySum(a))
+}
+
+func minSubarraySum(a []int) int {
+	min, minSoFar := 0, 0
+
+	for _, v := range a {
+		minSoFar = utils.Min(v, minSoFar+v)
+		min = utils.Min(min, minSoFar)
+	}
+	return min
+}
+
+func sum(a []int) int {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	return sum
+}
--- 01-arrays/03_calculate_maximum_subarray_sum_test.go
+++ 01-arrays/03_calculate_maximum_subarray_sum_test.go
@@ -17,3 +17,20 @@ func TestMaxSubarraySum(t *testing.T) {
 	output = maxSubarraySum(input)
 	assert.Equal(t, 0, output)
 }
+
+func TestMaxCircularSubarraySum(t *testing.T) {
+	// Example 1
+	input := []int{34, -50, 42, 14, -5, 86}
+	output := maxCircularSubarraySum(input)
+	assert.Equal(t, 171, output)
+
+	// Example 2
+	input = []int{-5, -1, -8, -9}
+	output = maxCircularSubarraySum(input)
+	assert.Equal(t, 0, output)
+
+	// Example 3
+	input = []int{8, -1, 3, 4}
+	output = maxCircularSubarraySum(input)
+	assert.Equal(t, 15, output)
+}
