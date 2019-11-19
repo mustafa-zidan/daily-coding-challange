6fc2b0783442972ef5c177ac9839b939b184924f
Mustafa Zidan <mustafa@zidan.me>
2019-11-19 21:50:41 +0100

    forth Daily coding problem
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- 01-arrays/04_find_smaller_elements_to_the_right.go
+++ 01-arrays/04_find_smaller_elements_to_the_right.go
@@ -5,6 +5,46 @@ package arrays
 // is the number of smaller element in the
 // original input array
 
+// Time O(N logN) Space O(N)
 func smallerCount(a []int) []int {
-	return nil
+	seen := make([]int, 1)
+	seen[0], a[len(a)-1] = a[len(a)-1], 0
+	for i := len(a) - 2; i >= 0; i-- {
+		index := getElementIndex(seen, a[i])
+		seen = append(seen[:index], append([]int{a[i]}, seen[index:]...)...)
+		a[i] = index
+	}
+	return a
+}
+
+// Return the element possible index in the array
+func getElementIndex(a []int, e int) int {
+	if len(a) == 0 {
+		return 0
+	}
+	min, max, pivot := 0, len(a), 0
+
+	for min < max {
+		if a[pivot] > e {
+			max = pivot - 1
+		} else {
+			min = pivot + 1
+		}
+		pivot = (min + max) / 2
+	}
+	return pivot
+}
+
+// Time O(n^2) Space O(1)
+func smallerCountBrute(a []int) []int {
+	for i := 0; i < len(a); i++ {
+		count := 0
+		for j := i + 1; j < len(a); j++ {
+			if a[i] > a[j] {
+				count++
+			}
+		}
+		a[i] = count
+	}
+	return a
 }
--- 01-arrays/04_find_smaller_elements_to_the_right_test.go
+++ 01-arrays/04_find_smaller_elements_to_the_right_test.go
@@ -3,12 +3,22 @@ package arrays
 import (
 	"testing"
 
+	"github.com/pkg/profile"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSmallerCount(t *testing.T) {
 	// Example 1
+	defer profile.Start().Stop()
 	input := []int{3, 4, 9, 6, 1}
 	output := smallerCount(input)
+	assert.Equal(t, []int{1, 1, 2, 1, 0}, output)
+}
+
+func TestSmallerCountBrute(t *testing.T) {
+	// Example 1
+	defer profile.Start().Stop()
+	input := []int{3, 4, 9, 6, 1}
+	output := smallerCountBrute(input)
 	assert.Equal(t, []int{1, 1, 2, 1, 0}, output)
 }
